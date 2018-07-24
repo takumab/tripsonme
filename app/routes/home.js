0117ceaeb7f6e545b4f2553a49a0ae33ace0b81b
Tacuma Bell <takumab@gmail.com>
2018-07-24 22:44:19 +0100

    Added images and component and manual data

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,28 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    let trips = [
+      {
+        title: 'Trip 1',
+        description: 'Here is Trip 1',
+        city: 'Lake Tahoe',
+        image: 'assets/images/lake.jpg'
+      },
+      {
+        title: 'Trip 2',
+        description: 'Here is Trip 2',
+        city: 'Rio de Janero',
+        image: 'assets/images/rio.jpg'
+      },
+      {
+        title: 'Trip 3',
+        description: 'Here is Trip 3',
+        city: 'Cairo',
+        image: 'assets/images/cairo.jpg'
+      },
+    ];
+
+    return trips;
+  }
 });
