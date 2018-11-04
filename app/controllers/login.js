14a99ad00ae037182a07514f68cdbf33fe111a69
Tacuma Bell <takumab@gmail.com>
2018-11-04 15:33:26 +0000

    Add login form and dummy submit button for now

---
+++ app/controllers/login.js
@@ -0,0 +1,14 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+export default Controller.extend({
+  session: service(),
+
+
+
+  actions: {
+    submit() {
+      console.log('logging in') // es-lint disable
+    }
+  }
+})
